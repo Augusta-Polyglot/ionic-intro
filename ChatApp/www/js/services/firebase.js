2ed11e2be0b811d03d5bef64d48a236bcf60d2ec
Justin Basinger <justin.m.basinger@gmail.com>
2014-12-30 23:12:13 -0500

    Adding some useful libraries and starting the firebase chat service

---
+++ ChatApp/www/js/services/firebase.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Justin on 12/30/2014.
+ */
+angular.module('firebase.chat',[]).factory('chat', [function(){
+  var chats = new Firebase('https://awesomenaut-studios.firebaseio.com/chats');
+  return {
+    sendMessage: function(name, message, errorCallback){
+      chats.push({name: name, message: message}, errorCallback ? errorCallback : function(){});
+    },
+    onMessageReceived: function(callback, errorCallback){
+      chats.on('child_added', function(snapshot) {
+        var chatData = snapshot.val();
+        callback(chatData.name, chatData.message);
+      }, function(errorObj){
+        if(errorCallback){
+          errorCallback(errorObj);
+        }
+      });
+    },
+    getPastMessages: function(callback){
+      chats.once("value", function(snapshot) {
+        callback(_.values(snapshot.val())[0]);
+      });
+    }
+  }
+}]);
